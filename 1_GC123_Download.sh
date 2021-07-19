bbe2de918bf8673f9246a2d116ad0bcabbd3d104
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-19 16:46:01 +0530

    Update 1_GC123_Download.sh

--- 1_GC123_Download.sh
+++ 1_GC123_Download.sh
@@ -11,7 +11,9 @@ wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Fun
 wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt.gz
 
 
-# To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal ( $ is Shell-prompt, must be excluded).
+# To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
+
+# ( $ is Shell-prompt; and # is Comment-prefix, Both must be excluded).
 
 # $ chmod a+x 1_GC123_Download.sh
 
