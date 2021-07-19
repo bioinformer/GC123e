5d5da998d67cd33e29a5fe8f68e2f62d8d2bac9c
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-19 16:30:40 +0530

    GC1% , GC2 % , GC3% and GC4d% Data for 1118 Spp.

---
+++ GC123_Dowsnload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_release_88.GCevobase.pc.download.GC4d.txt.gz
+
+wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Metazoa_release_35.GCevobase.pc.download.GC4d.txt.gz
+
+wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt.gz
+
+wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Fungi_release_35.GCevobase.pc.download.GC4d.txt.gz
+
+wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt.gz
