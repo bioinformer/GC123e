ac39abeee483e13676a3cc910cdad1d3bc1cf3f8
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-25 23:10:45 +0530

    Update 3_Extract_GC123_files.sh

--- 3_Extract_GC123_files.sh
+++ 3_Extract_GC123_files.sh
@@ -10,3 +10,12 @@ gunzip --keep Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt.gz
 
 gunzip --keep Ensembl_release_88.GCevobase.pc.download.GC4d.txt.gz
 
+# To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
+
+# ( $ is Shell-prompt; and # is Comment-prefix, Both must be excluded).
+ 
+# $ wget -c https://raw.githubusercontent.com/bioinformer/GC123e/main/1_GC123_Download.sh
+
+# $ chmod a+x 1_GC123_Download.sh
+
+# $ bash 1_GC123_Download.sh
