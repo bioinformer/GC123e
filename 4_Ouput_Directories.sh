e4a42fac1e98e73c225fa1319bbcc541313113b3
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-25 23:44:48 +0530

    Update 4_Ouput_Directories.sh

--- 4_Ouput_Directories.sh
+++ 4_Ouput_Directories.sh
@@ -17,3 +17,13 @@ cp Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt ./block3entropy/
 cp Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt ./block3entropy/protists
 
 cp Ensembl_release_88.GCevobase.pc.download.GC4d.txt ./block3entropy/release
+
+# To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
+
+# ( $ is Shell-prompt; and # is Comment-prefix, Both must be excluded).
+ 
+# $ wget -c https://raw.githubusercontent.com/bioinformer/GC123e/main/4_Ouput_Directories.sh
+
+# $ chmod a+x 4_Ouput_Directories.sh
+
+# $ bash 4_Ouput_Directories.sh
