e6755bfba6a8384d943af03e39d06c3650420377
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-25 23:10:18 +0530

    Create 3_Extract_GC123_files.sh

---
+++ 3_Extract_GC123_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gunzip --keep Ensembl_Fungi_release_35.GCevobase.pc.download.GC4d.txt.gz
+
+gunzip --keep Ensembl_Metazoa_release_35.GCevobase.pc.download.GC4d.txt.gz
+
+gunzip --keep Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt.gz
+
+gunzip --keep Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt.gz
+
+gunzip --keep Ensembl_release_88.GCevobase.pc.download.GC4d.txt.gz
+
