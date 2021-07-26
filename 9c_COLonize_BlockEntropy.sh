406c2c2c838cf577f90d8be9264beb4dbb894c29
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-26 11:51:56 +0530

    Update 9c_COLonize_BlockEntropy.sh

--- 9c_COLonize_BlockEntropy.sh
+++ 9c_COLonize_BlockEntropy.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+
+
+cd block3entropy
+
+cat ./fungi/fungi_5.txt | awk '{print $13}' | tee fungi.txt
+
+cat ./metazoa/metazoa_5.txt | awk '{print $13}' | tee metazoa.txt
+
+cat ./plants/plants_5.txt | awk '{print $13}' | tee plants.txt
+
+cat ./protists/protists_5.txt | awk '{print $13}' | tee protists.txt
+
+cat ./release/release_5.txt | awk '{print $13}' | tee release.txt
+
+
+
+
+
+
