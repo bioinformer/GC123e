a3217988cbeef07918114795f011b65515ce673a
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-26 10:53:15 +0530

    Update 1_GC123_Download.sh

--- 1_GC123_Download.sh
+++ 1_GC123_Download.sh
@@ -17,6 +17,10 @@ wget -c http://www.nextgenbioinformatics.org/GCevobase/download_data/Ensembl_Pro
 # To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
 
 # ( $ is Shell-prompt; and # is Comment-prefix, Both must be excluded).
+
+# Check that Current/ Present working Directory is "GCevoBase"
+# $ pwd
+# ~/path/to/GCevoBase
  
 # $ wget -c https://raw.githubusercontent.com/bioinformer/GC123e/main/1_GC123_Download.sh
 
