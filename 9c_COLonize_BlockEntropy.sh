4b5cc3a641758b4b5b4969bbca66e667cfde8402
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-26 11:52:30 +0530

    Update 9c_COLonize_BlockEntropy.sh

--- 9c_COLonize_BlockEntropy.sh
+++ 9c_COLonize_BlockEntropy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+# Check that Current/ Present working Directory is "GCevoBase"
+# $ pwd
+# ~/path/to/GCevoBase
 
 cd block3entropy
 
@@ -13,6 +15,8 @@ cat ./plants/plants_5.txt | awk '{print $13}' | tee plants.txt
 cat ./protists/protists_5.txt | awk '{print $13}' | tee protists.txt
 
 cat ./release/release_5.txt | awk '{print $13}' | tee release.txt
+
+
 
 
 
