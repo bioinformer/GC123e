f996b25687e100bd94479ffd11de03a70c672c90
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-30 11:31:03 +0530

    Get Triplet Codon BlockEntropy for Ensembl RELEASE

--- 9b_Release_GC123_b3ent.sh
+++ 9b_Release_GC123_b3ent.sh
@@ -19,7 +19,7 @@ cat release_3.txt | awk '{ for (i = 5; i <= 12; ++i) { $i = -($i)*(log($i)/log(2
 # ITERATION-5: Add-up All the Entropies to obtain Block Shannon Entropy as per the Summation-definition of Entropy.
 cat release_4.txt | awk '{$13 = ($5+$6+$7+$8+$9+$10+$11+$12); print $0}' | tee release_5.txt
 
-cd ../../../GCevoBase/
+cd ../../
 
 # To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
 
