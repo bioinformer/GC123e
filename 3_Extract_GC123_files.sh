3add5f27957b015dce6e165ff3229b0384cf7ce9
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-30 11:57:05 +0530

    Extract the 5 GunZip Compressed GC123 Text Files

--- 3_Extract_GC123_files.sh
+++ 3_Extract_GC123_files.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-cd GCevoBase
+cd GCevoBase ;
+
+sleep 1;
 
 # Check that Current/ Present working Directory is "GCevoBase"
 # $ pwd
 # ~/path/to/GCevoBase
 
-gunzip --keep Ensembl_Fungi_release_35.GCevobase.pc.download.GC4d.txt.gz
+gunzip --keep Ensembl_Fungi_release_35.GCevobase.pc.download.GC4d.txt.gz ;
+sleep 5;
 
-gunzip --keep Ensembl_Metazoa_release_35.GCevobase.pc.download.GC4d.txt.gz
+gunzip --keep Ensembl_Metazoa_release_35.GCevobase.pc.download.GC4d.txt.gz ;
+sleep 5;
 
-gunzip --keep Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt.gz
+gunzip --keep Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt.gz ;
+sleep 5;
 
-gunzip --keep Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt.gz
+gunzip --keep Ensembl_Protists_release_35.GCevobase.pc.download.GC4d.txt.gz ;
+sleep 5;
 
-gunzip --keep Ensembl_release_88.GCevobase.pc.download.GC4d.txt.gz
+gunzip --keep Ensembl_release_88.GCevobase.pc.download.GC4d.txt.gz ;
+sleep 5;
 
-cd ..
+cd .. ;
+sleep 1;
 
 # To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
 
