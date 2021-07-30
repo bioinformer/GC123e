18a22c0e4af76d9198e0a09821446cdc4ea4e70c
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-30 11:50:46 +0530

    Master Script to Prepare Final Data for 9d_Rscript

--- 0_TCBShE_Run_PipeLine.sh
+++ 0_TCBShE_Run_PipeLine.sh
@@ -3,26 +3,44 @@
 chmod a+x 1_GC123_Download.sh
 bash 1_GC123_Download.sh
 
+sleep 10
+
 chmod a+x 3_Extract_GC123_files.sh
 bash 3_Extract_GC123_files.sh
+
+sleep 10
 
 chmod a+x 4_Ouput_Directories.sh
 bash 4_Ouput_Directories.sh
 
+sleep 10
+
 chmod a+x 6_Fungi_GC123_b3ent.sh
 bash 6_Fungi_GC123_b3ent.sh
+
+sleep 10
 
 chmod a+x 7_Metazoa_GC123_b3ent.sh
 bash 7_Metazoa_GC123_b3ent.sh
 
+sleep 10
+
 chmod a+x 8_Plants_GC123_b3ent.sh
 bash 8_Plants_GC123_b3ent.sh
+
+sleep 10
 
 chmod a+x 9a_Protists_GC123_b3ent.sh
 bash 9a_Protists_GC123_b3ent.sh
 
+sleep 10
+
 chmod a+x 9b_Release_GC123_b3ent.sh
 bash 9b_Release_GC123_b3ent.sh
 
+sleep 10
+
 chmod a+x 9c_COLonize_BlockEntropy.sh
 bash 9c_COLonize_BlockEntropy.sh
+
+sleep 10
