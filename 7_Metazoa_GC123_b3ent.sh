b653199d5c3751731d1530c58df4a9de6d95c238
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-31 11:10:38 +0530

    Compute Triplet Codon BlockEntropy for METAZOA

--- 7_Metazoa_GC123_b3ent.sh
+++ 7_Metazoa_GC123_b3ent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ITERATION-1: Extract only GC1% , GC2% , GC3% and Exclude header-Line.
-cat Ensembl_Metazoa_release_35.GCevobase.pc.download.GC4d.txt | awk '{print $1,$7,$8,$9}' | grep -v "species_name" | tee metazoa_1.txt ;
+cat metazoa_0.txt | awk '{print $1,$7,$8,$9}' | grep -v "species_name" | tee metazoa_1.txt ;
 
 # ITERATION-2: Normalize the GC1% , GC2% , GC3% (percentage Values) to [0,1] inBetween RANGE, by dividing by 100 each
 cat metazoa_1.txt | awk '{$2 = ($2/100); $3 = ($3/100); $4 = ($4/100); print $0}' | tee metazoa_2.txt ;
