f633054f74d0ea68436fe340450dc064e45c85d4
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-31 11:03:11 +0530

    Compute Triplet Codon BlockEntropy for PLANTS

--- 8_Plants_GC123_b3ent.sh
+++ 8_Plants_GC123_b3ent.sh
@@ -1,25 +1,21 @@
 #!/bin/bash1
 
-cd block3entropy/plants
-
 # ITERATION-1: Extract only GC1% , GC2% , GC3% and Exclude header-Line.
-cat Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt | awk '{print $1,$7,$8,$9}' | grep -v "species_name" | tee plants_1.txt
+cat Ensembl_Plants_release_35.GCevobase.pc.fix.download.GC4d.txt | awk '{print $1,$7,$8,$9}' | grep -v "species_name" | tee plants_1.txt ;
 
 # ITERATION-2: Normalize the GC1% , GC2% , GC3% (percentage Values) to [0,1] inBetween RANGE, by dividing by 100 each
-cat plants_1.txt | awk '{$2 = ($2/100); $3 = ($3/100); $4 = ($4/100); print $0}' | tee plants_2.txt
+cat plants_1.txt | awk '{$2 = ($2/100); $3 = ($3/100); $4 = ($4/100); print $0}' | tee plants_2.txt ;
 
 # ITERATION-3: Calculate the 8 sub-rules/ Combinations of Triplet Codon position Probabilities, {000, 001, 010, 011, 100, 101, 110, 111} wherein:
 # 0(1) -> GC1 & 1(1) -> (1-GC1) ; 0(2) -> GC(2) & 1(2) -> (1-GC2) ; 0(3) -> GC(3) & 1(3) -> (1-GC3) given that for m(n), m=(0,1) Binary states & n=(1,2,3) index.
 cat plants_2.txt | awk '{$5=($2*$3*$4); $6=($2*$3*(1-$4)); $7=($2*(1-$3)*$4); $8=($2*(1-$3)*(1-$4)); $9=((1-$2)*$3*$4); $10=((1-$2)*$3*(1-$4)); \
-$11=((1-$2)*(1-$3)*$4); $12=((1-$2)*(1-$3)*(1-$4)); print $0}' | tee plants_3.txt
+$11=((1-$2)*(1-$3)*$4); $12=((1-$2)*(1-$3)*(1-$4)); print $0}' | tee plants_3.txt ;
 
 # ITERATION-4: Perform LOG transformation to compute Shannon entropy of the Individual "8" Combinations of the Triplet ("3") Block
-cat plants_3.txt | awk '{ for (i = 5; i <= 12; ++i) { $i = -($i)*(log($i)/log(2))}; print $0}' | tee plants_4.txt
+cat plants_3.txt | awk '{ for (i = 5; i <= 12; ++i) { $i = -($i)*(log($i)/log(2))}; print $0}' | tee plants_4.txt ;
 
 # ITERATION-5: Add-up All the Entropies to obtain Block Shannon Entropy as per the Summation-definition of Entropy.
-cat plants_4.txt | awk '{$13 = ($5+$6+$7+$8+$9+$10+$11+$12); print $0}' | tee plants_5.txt
-
-cd ../../
+cat plants_4.txt | awk '{$13 = ($5+$6+$7+$8+$9+$10+$11+$12); print $0}' | tee plants_5.txt ;
 
 # To RUN this Script, implement the 2 Commands below, preferably on Ubuntu 20.04 LTS Desktop Terminal 
 
