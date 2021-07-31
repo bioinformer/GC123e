e61c31126920be255f2635367424ac9a9fa0859d
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-07-31 13:00:59 +0530

    Compute Triplet Codon BlockEntropy for Ensembl RELEASE

--- 9b_Release_GC123_b3ent.sh
+++ 9b_Release_GC123_b3ent.sh
@@ -23,7 +23,7 @@ cat release_4.txt | awk '{$13 = ($5+$6+$7+$8+$9+$10+$11+$12); print $0}' | tee r
 
 # Check that Current/ Present working Directory is "GCevoBase"
 # $ pwd
-# ~/path/to/GCevoBase
+# ~/path/to/GC123e
  
 # $ wget -c https://raw.githubusercontent.com/bioinformer/GC123e/main/9b_Release_GC123_b3ent.sh
 
