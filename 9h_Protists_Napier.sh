d611bb011e3c58b74fd06e178ac84c1ef2ce3ff1
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-08-08 11:00:02 +0530

    Specific Transcripts for which Protists_TCBShE --> e, Napier Constant

---
+++ 9h_Protists_Napier.sh
@@ -0,0 +1,20 @@
+$ cat protists_7.txt | awk '$2 ~ /2.71828/' | tee protists_napier.txt
+
+Angomonas_deanei	2.71828
+Aphanomyces_invadans	2.71828
+Capsaspora_owczarzaki_atcc_30864	2.71828
+Eimeria_necatrix	2.71828
+Leishmania_mexicana_mhom_gt_2001_u1103	2.71828
+Leishmania_panamensis	2.71828
+Naegleria_gruberi	2.71828
+Paramecium_tetraurelia	2.71828
+Phytophthora_parasitica_p1976	2.71828
+Phytophthora_sojae	2.71828
+Pythium_arrhenomanes	2.71828
+Pythium_iwayamai	2.71828
+Trichomonas_vaginalis_g3	2.71828
+Trichomonas_vaginalis_g3	2.71828
+Trypanosoma_rangeli_sc58	2.71828
+Hyaloperonospora_arabidopsidis	2.71828
+Leptomonas_seymouri	2.71828
+Saprolegnia_diclina_vs20	2.71828
