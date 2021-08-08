5bd76de6c11800cf4693bce6f4988bed3ceab07d
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-08-08 10:54:51 +0530

    Specific Transcripts for which Metazoa_TCBShE --> e, Napier Constant

---
+++ 9f_Metazoa_Napier.sh
@@ -0,0 +1,21 @@
+$ cat metazoa_7.txt | awk '$2 ~ /2.71828/' | tee metazoa_napier.txt
+
+Culex_quinquefasciatus	2.71828
+Drosophila_pseudoobscura	2.71828
+Ixodes_scapularis	2.71828
+Ixodes_scapularis	2.71828
+Loa_loa	2.71828
+Lottia_gigantea	2.71828
+Nasonia_vitripennis	2.71828
+Octopus_bimaculoides	2.71828
+Pediculus_humanus	2.71828
+Solenopsis_invicta	2.71828
+Stegodyphus_mimosarum	2.71828
+Thelohanellus_kitauei	2.71828
+Acyrthosiphon_pisum	2.71828
+Acyrthosiphon_pisum	2.71828
+Adineta_vaga	2.71828
+Amphimedon_queenslandica	2.71828
+Amphimedon_queenslandica	2.71828
+Anoplophora_glabripennis	2.71828
+Atta_cephalotes	2.71828
