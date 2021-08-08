251522f538daea7c03fa0407d734aafd7b8c5fca
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-08-08 10:57:57 +0530

    Specific Transcripts for which Plants_TCBShE --> e, Napier Constant

---
+++ 9g_Plants_Napier.sh
@@ -0,0 +1,12 @@
+$ cat plants_7.txt | awk '$2 ~ /2.71828/' | tee plants_napier.txt
+
+Oryza_glumaepatula	2.71828
+Oryza_punctata	2.71828
+Oryza_rufipogon	2.71828
+Selaginella_moellendorffii	2.71828
+Selaginella_moellendorffii	2.71828
+Selaginella_moellendorffii	2.71828
+Aegilops_tauschii	2.71828
+Arabidopsis_thaliana	2.71828
+Arabidopsis_thaliana	2.71828
+Beta_vulgaris	2.71828
