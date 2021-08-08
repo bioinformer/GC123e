7c050465501214a985d2fce78fc12ab90526d518
Praharshit Sharma <praharshit.sharma@gmail.com>
2021-08-08 11:02:49 +0530

    Specific Transcripts for which Vertebra_TCBShE --> e, Napier Constant

---
+++ 9i_Vertebra_Napier.sh
@@ -0,0 +1,16 @@
+$ cat release_7.txt | awk '$2 ~ /2.71828/' | tee vertebra_napier.txt
+
+Callithrix_jacchus	2.71828
+Felis_catus	2.71828
+Homo_sapiens	2.71828
+Mus_musculus_129s1svimj	2.71828
+Mus_musculus_lpj	2.71828
+Mus_musculus_c57bl6nj	2.71828
+Mus_musculus_nzohlltj	2.71828
+Mustela_putorius_furo	2.71828
+Myotis_lucifugus	2.71828
+Oryctolagus_cuniculus	2.71828
+Otolemur_garnettii	2.71828
+Sus_scrofa	2.71828
+Takifugu_rubripes	2.71828
+Tarsius_syrichta	2.71828
