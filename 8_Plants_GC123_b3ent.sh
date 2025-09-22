d7806ab6025aadfb291e77b9bfe6cc6e6232d1f2
Praharshit Sharma <praharshit.sharma@gmail.com>
2025-09-22 18:44:48 +0530

    Update 8_Plants_GC123_b3ent.sh

--- 8_Plants_GC123_b3ent.sh
+++ 8_Plants_GC123_b3ent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash1
 
 # ITERATION-1: Extract only GC1% , GC2% , GC3% and Exclude header-Line.
-cd ../plants
+cd ./tcbshe/plants
 cat plants_gc123.txt | awk '{print $1,$7,$8,$9}' | grep -v "species_name" | tee plants_step1.txt ;
 
 # ITERATION-2: Normalize the GC1% , GC2% , GC3% (percentage Values) to [0,1] inBetween RANGE, by dividing by 100 each
